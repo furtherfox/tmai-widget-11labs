1dbca732a87375a15ff1f58a735d024aa7ba36a0
furtherfox <foxwill@gmail.com>
2023-11-23 21:33:29 -0600

    Update embedWidget.js with fixed filenames

--- public/embedWidget.js
+++ public/embedWidget.js
@@ -4,16 +4,24 @@
     widgetContainer.id = 'widget-root'; // Use a unique ID
     document.body.appendChild(widgetContainer);
 
+    // Use fixed filenames for CSS and JavaScript files
+    var cssFilename = 'main.css';
+    var jsFilename = 'main.js';
+
+    // Construct the URLs with fixed filenames
+    var cssUrl = 'https://tmai-widget.vercel.app/static/css/' + cssFilename;
+    var jsUrl = 'https://tmai-widget.vercel.app/static/js/' + jsFilename;
+
     // Append CSS
     var css = document.createElement('link');
-    css.href = 'https://tmai-widget.vercel.app/static/css/main.6d0d6734.css';
+    css.href = cssUrl;
     css.rel = 'stylesheet';
     css.type = 'text/css';
     document.head.appendChild(css);
 
     // Append JavaScript
     var script = document.createElement('script');
-    script.src = 'https://tmai-widget.vercel.app/static/js/main.0b046156.js';
+    script.src = jsUrl;
     script.async = true;
     document.body.appendChild(script);
 })();
