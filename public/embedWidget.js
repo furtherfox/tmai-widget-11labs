72dcd040bb110e5d0dbd7643199f52e9337fd9ee
furtherfox <foxwill@gmail.com>
2023-11-23 21:41:19 -0600

    Update embedWidget.js with fixed filenames

--- public/embedWidget.js
+++ public/embedWidget.js
@@ -4,24 +4,34 @@
     widgetContainer.id = 'widget-root'; // Use a unique ID
     document.body.appendChild(widgetContainer);
 
-    // Use fixed filenames for CSS and JavaScript files
-    var cssFilename = 'main.css';
-    var jsFilename = 'main.js';
+    // Fetch the asset manifest
+    fetch('/asset-manifest.json') // Update with the actual path
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // Get the actual filenames for CSS and JavaScript from the manifest
+        var cssFilename = data['static/css/main.css'];
+        var jsFilename = data['static/js/main.js'];
 
-    // Construct the URLs with fixed filenames
-    var cssUrl = 'https://tmai-widget.vercel.app/static/css/' + cssFilename;
-    var jsUrl = 'https://tmai-widget.vercel.app/static/js/' + jsFilename;
+        // Construct the URLs with the retrieved filenames
+        var cssUrl = 'https://tmai-widget.vercel.app/' + cssFilename;
+        var jsUrl = 'https://tmai-widget.vercel.app/' + jsFilename;
 
-    // Append CSS
-    var css = document.createElement('link');
-    css.href = cssUrl;
-    css.rel = 'stylesheet';
-    css.type = 'text/css';
-    document.head.appendChild(css);
+        // Append CSS
+        var css = document.createElement('link');
+        css.href = cssUrl;
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        document.head.appendChild(css);
 
-    // Append JavaScript
-    var script = document.createElement('script');
-    script.src = jsUrl;
-    script.async = true;
-    document.body.appendChild(script);
+        // Append JavaScript
+        var script = document.createElement('script');
+        script.src = jsUrl;
+        script.async = true;
+        document.body.appendChild(script);
+    })
+    .catch(function(error) {
+        console.error('Error fetching asset manifest:', error);
+    });
 })();
