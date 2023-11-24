c9bf0bc26b3aa60c3ce077a43ea37086aed7cf84
furtherfox <foxwill@gmail.com>
2023-11-23 21:53:13 -0600

    Update embedWidget.js with fixed filenames

--- public/embedWidget.js
+++ public/embedWidget.js
@@ -4,19 +4,19 @@
     widgetContainer.id = 'widget-root'; // Use a unique ID
     document.body.appendChild(widgetContainer);
 
-    // Fetch the asset manifest
-    fetch('/asset-manifest.json') // Update with the actual path
+    // Fetch the asset manifest from the correct path (inside the "build" folder)
+    fetch('/build/asset-manifest.json') // Update with the correct path
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         // Get the actual filenames for CSS and JavaScript from the manifest
-        var cssFilename = data['static/css/main.css'];
-        var jsFilename = data['static/js/main.js'];
+        var cssFilename = data.files['main.css'];
+        var jsFilename = data.files['main.js'];
 
         // Construct the URLs with the retrieved filenames
-        var cssUrl = 'https://tmai-widget.vercel.app/' + cssFilename;
-        var jsUrl = 'https://tmai-widget.vercel.app/' + jsFilename;
+        var cssUrl = 'https://tmai-widget.vercel.app' + cssFilename;
+        var jsUrl = 'https://tmai-widget.vercel.app' + jsFilename;
 
         // Append CSS
         var css = document.createElement('link');
