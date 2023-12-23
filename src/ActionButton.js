f2d1f4555b89dea7cd6a8bc804cbb84c304278fa
furtherfox <foxwill@gmail.com>
2023-12-23 17:09:25 -0600

    selected langauge text fix

--- src/ActionButton.js
+++ src/ActionButton.js
@@ -3,20 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'; // Import faVolumeHigh
 import styles from './Widget.module.css';
 
-const ActionButton = ({ state, onClick }) => {
+// Use <FontAwesomeIcon icon={faVolumeHigh} /> instead of <FontAwesomeIcon icon={faHeadphones} />
+const ActionButton = ({ state, onClick, playText, stopText }) => {
+  let displayText;
+  if (state === 'loading') {
+    // Replace this with a spinner or any loading indicator
+    displayText = <div className={styles['myWidget-spinner']}></div>;
+  } else {
+    displayText = state === 'stop' ? stopText : playText;
+  }
+
   return (
     <button onClick={onClick} className={styles['myWidget-actionButton']}>
-      <FontAwesomeIcon icon={faVolumeHigh} className={styles['myWidget-icon']} /> 
+      <FontAwesomeIcon icon={faVolumeHigh} className={styles['myWidget-icon']} /> {/* Updated icon */}
       <span className={styles['myWidget-actionButtonText']}>
-        {state === 'loading'
-          ? <div className={styles['myWidget-spinner']}></div>
-          : state === 'stop'
-          ? 'Stop'
-          : 'Tell me about it'}
+        {displayText}
       </span>
     </button>
   );
 };
-
 
 export default ActionButton;
